4ba6805235a6c3db7b5e955cddaf0390fe6b453c
bachana707 <bachana707@users.noreply.github.com>
2024-08-26 16:22:27 +0400

    ShopItem

---
+++ Assets/BSK/Scripts/Shop/ShopItemInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItemInfo : MonoBehaviour
+{
+    public Image Icon;
+    public Image selectImage;
+    public Image lockedImage;
+
+    [Space(5)]
+    public Button button;
+    public Text priceText;
+
+
+    [HideInInspector] public bool unlocked = false;
+    [HideInInspector] public int index;
+    [HideInInspector] public int price;
+
+
+    public void Refresh_UI()
+    {
+        priceText.text = price.ToString() + "$"; //MoneyCount.ConvertMoney(price);
+    }
+
+   
+
+    public void Unlock()
+    {
+        unlocked = true;
+        SetData();
+    }
+
+    public void getData()
+    {
+        if (!unlocked)
+        {
+            unlocked = ShopManager.GetShopItemUnlockStatus(index);
+        }
+        else
+        {
+            ShopManager.SetShopItemUnlockStatus(index, unlocked);
+        }
+
+
+       // GameManager.GetInstance().shopManager.shopItems[tabType].TabData[index].unlocked = unlocked;
+    }
+
+    public void SetData()
+    {
+      //  GameManager.GetInstance().shopManager.shopItems[tabType].TabData[index].unlocked = unlocked;
+        ShopManager.SetShopItemUnlockStatus(index, unlocked);
+    }
+
+}
---
+++ Assets/BSK/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public enum shopItemStatus
+{
+    bought, selected, locked
+}
+
+
+
+public class ShopManager : MonoBehaviour
+{
+
+    public GameObject ItemUIPrefab;
+ 
+ 
+    
+
+    public static bool GetShopItemUnlockStatus(int index)
+    {
+        return PlayerPrefs.GetInt(Constants.ItemUnlockStatusPlayerPrefsTag + index.ToString(), 0) == 1;
+    }
+    public static void SetShopItemUnlockStatus(int index, bool unlocked)
+    {
+        PlayerPrefs.SetInt(Constants.ItemUnlockStatusPlayerPrefsTag +index.ToString(), unlocked ? 1 : 0);
+    }
+
+
+}
+[System.Serializable]
+public class ShopItemData
+{
+    public string Name;
+
+    [Space(10)]
+    public GameObject prefab;
+    //public bool DisplayPreview = false;
+    public Sprite UI_Icon;
+
+    [Space(10)]
+    public bool unlocked;
+    public int price;
+}
+
+
